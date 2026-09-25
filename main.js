7585f2e3bd54a5f19acf05ba7bb73dbcc43e030e
Gear <Gear70704@gmail.com>
2026-09-24 22:58:06 -0400

    Chapter selection started

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const currentBookWindow = document.getElementById("bookContainer");
 
+const book1 = document.querySelector(".chapter-selector-book1");
+
 // books
 const bookOneButton = document.getElementById("bookOne");
 const bookTwoButton = document.getElementById("bookTwo");
 
 bookOneButton.addEventListener('click', () => {
     currentBookWindow.src = "pages/bookOne.html";
+    book1.classList.add("show"); // chapter selector
 });
 
 bookTwoButton.addEventListener('click', () => {
     currentBookWindow.src = "pages/bookTwo.html";
+    currentBook = "bookTwo";
 });
