bae991bb4a7efb96612cc15c8f38e727bb3e0bc6
Gear <Gear70704@gmail.com>
2026-09-25 22:10:43 -0400

    character bios completed

--- main.js
+++ main.js
@@ -2,90 +2,107 @@ const currentBookWindow = document.getElementById("bookContainer");
 
 const homeButton = document.getElementById("homeButton");
 const biosButton = document.getElementById("biosButton");
-const worshipButton = document.getElementById("worshipButton");
 
-const book1 = document.querySelector(".chapter-selector-book1");
+const book1Chapters = document.querySelector(".chapter-selector-book1");
 const book1Buttons = document.querySelectorAll(".chapter-selector-book1 .chapterButton");
 
-const book2 = document.querySelector(".chapter-selector-book2");
+const book2Chapters = document.querySelector(".chapter-selector-book2");
 const book2Buttons = document.querySelectorAll(".chapter-selector-book2 .chapterButton");
 
-const book3 = document.querySelector(".chapter-selector-book3");
+const book3Chapters = document.querySelector(".chapter-selector-book3");
 const book3Buttons = document.querySelectorAll(".chapter-selector-book3 .chapterButton");
 
-// books
-const bookOneButton = document.getElementById("bookOne");
-const bookTwoButton = document.getElementById("bookTwo");
-const bookThreeButton = document.getElementById("bookThree");
+// ------------------------------------------
+function hideAllButtons()
+{
+    document.querySelector(".books").classList.add("hidden");
+
+    book1Chapters.classList.remove("show");
+    book2Chapters.classList.remove("show");
+    book3Chapters.classList.remove("show");
+};
+function hideChapters()
+{
+    book1Chapters.classList.remove("show");
+    book2Chapters.classList.remove("show");
+    book3Chapters.classList.remove("show");
+}
+function showBookButtons()
+{
+    document.querySelector(".books").classList.remove("hidden");
+};
+// ---------------------------------------------
 
 // HOME CHECK
-homeButton.addEventListener("click", () => {
-    currentBookWindow.src = "pages/cedarismDefinition.html";
-
-    book1.classList.remove("show");
-    book2.classList.remove("show");
-    book3.classList.remove("show");
-});
-biosButton.addEventListener("click", () => {
-    currentBookWindow.src = "pages/bios.html";
-
-    book1.classList.remove("show");
-    book2.classList.remove("show");
-    book3.classList.remove("show");
-});
-worshipButton.addEventListener("click", () => {
-    currentBookWindow.src = "pages/worship.html";
-
-    book1.classList.remove("show");
-    book2.classList.remove("show");
-    book3.classList.remove("show");
-});
-
-// BOOK CHECK
-bookOneButton.addEventListener("click", () => {
-    currentBookWindow.src = "pages/bookOne/ch1.html";
-    book1.classList.add("show"); // show book1 chapters
-
-    book2.classList.remove("show"); // hide book2 chapters
-    book3.classList.remove("show"); // yk already
-});
-bookTwoButton.addEventListener("click", () => {
-    currentBookWindow.src = "pages/bookTwo/ch1.html";
-    book2.classList.add("show"); // show book2 chapters
-
-    book1.classList.remove("show"); // hide book1 chapters
-    book3.classList.remove("show");
-});
-bookThreeButton.addEventListener("click", () => {
-    currentBookWindow.src = "pages/bookThree/ch1.html";
-    book3.classList.add("show");
-
-    book1.classList.remove("show");
-    book2.classList.remove("show");
-})
-
-// chapter selector
-book1Buttons.forEach((button) => 
-{
+function homeChecker(button, page){
     button.addEventListener("click", () => 
     {
-        const chNumber = button.id.split("-")[1]; // gets number from selector-1-b1 or similar
-        currentBookWindow.src = `pages/bookOne/ch${chNumber}.html`;
+        if (page == "home")
+        {
+            currentBookWindow.src = `pages/${page}.html`;
+
+            showBookButtons();
+            hideChapters();
+        }   
+        else if (page == "bios")
+        {
+            currentBookWindow.src = `pages/${page}.html`;
+            
+            hideAllButtons();
+        } 
     });
-});
-book2Buttons.forEach((button) =>
-{
-    button.addEventListener("click", () =>
+};
+
+homeChecker(document.getElementById("homeButton"), "home");
+homeChecker(document.getElementById("biosButton"), "bios");
+
+// BOOK CHECK
+function bookChecker(button, book){
+    button.addEventListener("click", () => 
     {
-        const chNumber = button.id.split("-")[1];
-        currentBookWindow.src = `pages/bookTwo/ch${chNumber}.html`;
+        if (book == "bookOne")
+        {
+            currentBookWindow.src = `pages/${book}/ch1.html`;
+            book1Chapters.classList.add("show");
+
+            book2Chapters.classList.remove("show");
+            book3Chapters.classList.remove("show");
+        }   
+        else if (book == "bookTwo")
+        {
+            currentBookWindow.src = `pages/${book}/ch1.html`;
+            book2Chapters.classList.add("show");
+
+            book1Chapters.classList.remove("show");
+            book3Chapters.classList.remove("show"); 
+        } 
+        else if (book == "bookThree")
+        {
+            currentBookWindow.src = `pages/${book}/ch1.html`;
+            book3Chapters.classList.add("show");
+
+            book1Chapters.classList.remove("show");
+            book2Chapters.classList.remove("show");
+        } 
     });
-});
-book3Buttons.forEach((button) =>
-{
-    button.addEventListener("click", () =>
+};
+
+bookChecker(document.getElementById("bookOne"), "bookOne");
+bookChecker(document.getElementById("bookTwo"), "bookTwo");
+bookChecker(document.getElementById("bookThree"), "bookThree");
+
+// chapter selector
+function chatperSelector(buttons, book){
+    buttons.forEach((button) => 
     {
-        const chNumber = button.id.split("-")[1];
-        currentBookWindow.src = `pages/bookThree/ch${chNumber}.html`;
+        button.addEventListener("click", () => 
+        {
+            const chNumber = button.id.split("-")[1];
+            currentBookWindow.src = `pages/${book}/ch${chNumber}.html`;
+        });
     });
-});
+};
+
+chatperSelector(book1Buttons, "bookOne");
+chatperSelector(book2Buttons, "bookTwo");
+chatperSelector(book3Buttons, "bookThree");
